fc37b818371bd18335018a61d721a604e6610884
unknown <>
2016-08-22 21:14:30 +0800

    qianduan

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $("#nav li").hover(
-        function() {
-            $(this).find("ul").show(100);},
-        function() {
-            $(this).find("ul").hide(300);}
-	);
-});
